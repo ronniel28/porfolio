f8df337dd864e1a40e2e458ef0055aeaf9b57faa
ronnield <ronniel.appleid@gmail.com>
2024-12-26 14:34:21 +0800

    add backend api, send email

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,32 +1,59 @@
-import styles from './ContactStyles.module.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './ContactStyles.module.css';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`${import.meta.env.VITE_API_URL}/send-email`, formData)
+      .then(response => {
+        console.log('Email sent successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('There was an error sending the email:', error);
+      });
+  };
+
   return (
     <section id='contact' className={styles.container}>
         <h1 className='sectionTitle'>Contact</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div className='formGroup'>
                 <label htmlFor="name" hidden>
                     Name
                 </label>
-                <input type="text" name="name" id="name" placeholder="Name" required/>
+                <input type="text" name="name" id="name" placeholder="Name" required onChange={handleChange} />
             </div>
             <div className='formGroup'>
                 <label htmlFor="email" hidden>
                     Email
                 </label>
-                <input type="text" name="email" id="email" placeholder="Email" required/>
+                <input type="email" name="email" id="email" placeholder="Email" required onChange={handleChange} />
             </div>
             <div className='formGroup'>
                 <label htmlFor="message" hidden>
-                    Name
+                    Message
                 </label>
-                <textarea name="message" id="message" placeholder="Message" required></textarea>
+                <textarea name="message" id="message" placeholder="Message" required onChange={handleChange}></textarea>
             </div>
             <input type="submit" className='hover btn' value="Submit"/>
         </form>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
